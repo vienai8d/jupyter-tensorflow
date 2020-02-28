79d505e2eedd60606869f2d66dd84e90cd873636
vienai8d <31370233+vienai8d@users.noreply.github.com>
2020-02-28 23:40:12 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ CONTAINER_NAME=$IMAGE_NAME
 LOCAL_DIR=$HOME/tf
 
 docker build . -t $IMAGE_NAME
-docker run -d --rm --name $CONTAINER_NAME -u $(id -u):$(id -g) -p 8888:8888 -v $LOCAL_DIR:/tf/local $IMAGE_NAME
+docker run -d --rm --name $CONTAINER_NAME -u $(id -u):$(id -g) -p 8888:8888 -p 9999:9999 -v $LOCAL_DIR:/tf/local $IMAGE_NAME
 
 sleep 1
 
